bfe6a933baf0cb28986578b476afa3693a49d6fd
sbx_user1051 <sbx_user1051@169.254.4.5>
2025-03-07 06:57:50 +0000

    Fri Mar 07 06:57:50 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -16,7 +16,7 @@ def checkout(skus):
     for item, value in counter:
         if item=="B" and value % 2 == 0:
             cost += (value // 2) * 45
-        if item=="B" and value % 2 != 0:
+        if item=="B" and value % 2 != 0 and value > 2:
             cost += (value % 2)
 
 
@@ -27,6 +27,7 @@ def checkout(skus):
 
 
     return skus
+
 
 
 
