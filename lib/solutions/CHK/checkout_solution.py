3adbae9263c747fccb802b5a325932aaf8f33f6a
sbx_user1051 <sbx_user1051@169.254.4.5>
2025-03-07 06:44:06 +0000

    Fri Mar 07 06:44:06 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,14 +2,20 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-lookup = {"A": 50, "B": 30, "C": 20, "D": 15}
+
+
+# lookup = {"A": 50, "B": 30, "C": 20, "D": 15}
+from collections import Counter
 
 def checkout(skus):
     if not skus:
         return -1
 
-    item_prices = []
-    for sku in skus:
+    counter = Counter(skus)
+
+    for item in counter.items():
+
 
 
     return skus
+
