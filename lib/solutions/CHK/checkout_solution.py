3bed15fcc20f2c472744de886a7b1de3bbd5998d
sbx_user1051 <sbx_user1051@169.254.4.5>
2025-03-07 06:35:06 +0000

    Fri Mar 07 06:35:06 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,15 +2,18 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-lookup = {}
+lookup = {"A": 50, "B": 30, "C": 20, "D": 15}
+
 def checkout(skus):
     if not skus:
         return -1
 
+    item_prices = []
     for sku in skus:
 
 
     return skus
+
 
 
 
